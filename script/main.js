aed6f43dca51eaa5c91af1d9eca455d4d1dac318
Fabio Angelici <fabio.angelici@outlook.it>
2022-04-24 22:40:39 +0200

    Aggiunta sezione Thank you, completata

--- script/main.js
+++ script/main.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-undef */
 
 const listItems = document.querySelectorAll('.value');
+const submitButton = document.querySelector('#submit-button');
+const mainPage = document.querySelector('#main-page');
+const thankyouPage = document.querySelector('#thankyou-page');
+const finalValutationText = document.querySelector('#final-valutation');
+let userValutation = 0;
 
 listItems.forEach((el) => {
     el.addEventListener('click', () => {
@@ -12,9 +17,17 @@ listItems.forEach((el) => {
                 activeItem.classList.remove('active'); // * remove active class and ...
             });
             el.classList.add('active'); // * ...add active class to the element clicked by the user
+            userValutation = el.value;
         } else {
             // * Otherwise
             el.classList.add('active'); // * add active class to the element clicked by the user
+            userValutation = el.value;
         }
     });
+});
+
+submitButton.addEventListener('click', () => {
+    mainPage.classList.toggle('hidden');
+    thankyouPage.classList.toggle('hidden');
+    finalValutationText.innerHTML = `You selected ${userValutation} out of 5`;
 });
